716e763da656ff227dc36d8a8606e03a0f618399
Max Anderson <cctpmax@gmail.com>
2018-05-28 19:00:14 -0400

    Added a function for Setting the API Key

---
+++ PoSH-Translator/Public/Set-GoogleTranslateAPIKey.ps1
@@ -0,0 +1,22 @@
+Function Set-GoogleTranslateAPIKey {
+
+    [CmdletBinding()]
+    Param (
+
+        [Parameter(Mandatory=$True,Position=0,ValueFromPipeline=$True)]
+        [String]$APIKey
+
+    )
+
+    $Key = [PSCustomObject]@{
+        
+        "APIKey" = $APIKey
+    
+    }
+
+    $ModulePath = Get-Module -Name PoSH-Translator | Select-Object -ExpandProperty ModuleBase
+    $KeyFileName = "key.json"
+
+    $Key | ConverTo-JSON | Out-File -FilePath "$ModulePath\$KeyFileName" -Force
+
+}
