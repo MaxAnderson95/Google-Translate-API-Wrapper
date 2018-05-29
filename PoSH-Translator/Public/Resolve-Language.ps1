11b899efa1807a50beb8eff47f70c6e35d146133
unknown <cctpmax@gmail.com>
2018-05-28 23:00:50 -0400

    Added array and pipeline support

--- PoSH-Translator/Public/Resolve-Language.ps1
+++ PoSH-Translator/Public/Resolve-Language.ps1
@@ -3,19 +3,35 @@ Function Resolve-Language {
     [CmdletBinding()]
     Param (
 
-        [Parameter(Mandatory=$True,Position=0)]
-        [String]$SourceText
+        [Parameter(Mandatory=$True,Position=0,ValueFromPipeline=$True)]
+        [String[]]$SourceText
 
     )
 
-    $Body = @{
+    Begin {
 
-        "q" = $SourceText
+    }
+    
+    Process {
+
+        ForEach ($String in $SourceText) {
+        
+            $Body = @{
+
+                "q" = $SourceText
+
+            }
+
+            $Response = Invoke-GoogleTranslateAPIRequest -Method POST -Resource detect -Body $Body
+
+            Write-Output $Response
+
+        }
 
     }
 
-    $Response = Invoke-GoogleTranslateAPIRequest -Method POST -Resource detect -Body $Body
+    End {
 
-    Write-Output $Response
+    }
 
 }
