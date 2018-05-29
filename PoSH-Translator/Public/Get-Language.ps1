dd5ebf66c08854ae1066bd9355bacdfb25105e3a
unknown <cctpmax@gmail.com>
2018-05-28 22:37:15 -0400

    Wrote the Get-Language function

--- PoSH-Translator/Public/Get-Language.ps1
+++ PoSH-Translator/Public/Get-Language.ps1
@@ -3,8 +3,18 @@ Function Get-Language {
     [CmdletBinding()]
     Param (
 
+        [String]$LocalizedResultsLanguageCode = 'en'
+
     )
 
-    
+    $Body = @{
+
+        'target' = $LocalizedResultsLanguageCode
+
+    }
+
+    $Response = Invoke-GoogleTranslateAPIRequest -Method GET -Resource languages -Body $Body
+
+    Write-Output $Response.data.languages | Select @{name='LanguageCode';expression='language'}, @{name='LanguageName';expression='name'}
 
 }
